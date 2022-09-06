5dea16591f97bb262049b84a4376af0c39d638ae
Alex Moreira <amoreira@N092.annex>
2022-09-06 15:54:40 -0300

    ajustes deploy Amazon v2

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Job, Gallery
 
-# Register your models here.
+@admin.register(Job)
+class ClientsAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'title')
+
+    fieldsets = (
+        ('Dados', {'fields': ('title', 'date', 'tags', 'img')}),
+    )
+
+@admin.register(Gallery)
+class ClientsAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'job', 'order')
+
+    fieldsets = (
+        ('Dados', {'fields': ('job', 'order', 'img')}),
+    )
