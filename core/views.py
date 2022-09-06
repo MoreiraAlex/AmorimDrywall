67155d03245c614e36f39e6f75e662b3799e4c35
Alex Moreira <amoreira@N092.annex>
2022-09-06 16:21:52 -0300

    ajustes deploy Amazon v4

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import Job, Gallery
 
+from django.conf import settings
+
 def index(request):
     jobs = Job.objects.all().order_by('-id')
     gallery = Gallery.objects.all().order_by('order')
+    debug = settings.DEBUG
     
-    return render(request, 'index.html', {'jobs': jobs, 'photos': gallery})
+    return render(request, 'index.html', {'jobs': jobs, 'photos': gallery, 'debug': debug})
