92f910a6d9f44914f10fb86f3b417fa30a9cf4ed
MoreiraAlex <celerom100@gmail.com>
2022-10-23 19:10:42 -0300

    Adicionando responsividade na area administrativa

--- frontend/pages/admin.js
+++ frontend/pages/admin.js
@@ -14,44 +14,14 @@ export default function Admin() {
     const[refresh, setRefresh] = useState(false)
     const[files, setFiles] = useState([])
     const[photos, setPhotos] = useState([])
-    const[editPhotos, setEditPhotos] = useState([])
     const[length, setLength] = useState(-1)
     const[img, setImg] = useState([])
-    const[editData, setEditData] = useState([])
-
 
 
     const closeModal = (e) => {
         if(e.target.id == 'canClose'){
             setModal(false)
         }
-    }
-
-    function removeImg(action, key){
-        if(action == 'remove'){
-            const index = editData.photos.findIndex(photo => photo[0] == key)
-
-            if(index >= 0){
-                editData.photos.splice(index, 1)
-            }
-
-        } else if (action == 'add'){
-            editData.photos.push(key)
-        }
-
-    }
-
-    function EditJob(id){
-
-        api.get(`job/${id}`, data)
-        .then(response => {
-            setEditData(response.data)
-            setModal(true)
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-
     }
 
     function DeleteJob(id, img, urls){
@@ -86,75 +56,68 @@ export default function Admin() {
         })
     }
 
-    function DataForm(data, method){
+    function DataForm(data){
         data.preventDefault();
         
-        if(method == 'post'){
+        setFiles([])
+        Object.values(data.target).map((value) => {
+            setFiles(oldArray => [...oldArray, value.value])
+        })
 
-            setFiles([])
-            Object.values(data.target).map((value) => {
-                setFiles(oldArray => [...oldArray, value.value])
-            })
-    
-    
-            const formData = new FormData();
-    
-            formData.set('file', data.target[2].files[0])
-    
+        const formData = new FormData();
+
+        formData.set('file', data.target[2].files[0])
+
+        api.post('upload', formData)
+        .then(response => {
+            setImg([response.data.key, response.data.url])
+        })
+        .catch((error) => {
+            console.log(error)
+        }) 
+        
+
+        setLength(data.target[3].files.length)
+
+        setPhotos([])
+
+        Object.values(data.target[3].files).map(element => {
+
+            formData.set('file', element)
+
             api.post('upload', formData)
             .then(response => {
-                setImg([response.data.key, response.data.url])
+                setPhotos(oldArray => [...oldArray, [response.data.key, response.data.url]])
             })
             .catch((error) => {
                 console.log(error)
-            }) 
-            
-    
-            setLength(data.target[3].files.length)
-    
-            setPhotos([])
-    
-            Object.values(data.target[3].files).map(element => {
-    
-                formData.set('file', element)
-    
-                api.post('upload', formData)
-                .then(response => {
-                    setPhotos(oldArray => [...oldArray, [response.data.key, response.data.url]])
-                })
-                .catch((error) => {
-                    console.log(error)
-                })
             })
-        } else if (method == 'put'){
-
-        }
-
+        })
 
         setModal(false)
     }
 
-    // useEffect(() => {
-    //     if(length == photos.length && img.length != 0){
-    //         const form = {
-    //             name: files[0],
-    //             desc: files[1],
-    //             img: img,
-    //             photos: photos
-    //         }
+    useEffect(() => {
+        if(length == photos.length && img.length != 0){
+            const form = {
+                name: files[0],
+                desc: files[1],
+                img: img,
+                photos: photos
+            }
 
-    //         api.post('job', form)
-    //         .then(response => {
-    //             console.log(response.data)
-    //             setLength(-1)
-    //             setImg([])
-    //             refresh ? setRefresh(false) : setRefresh(true)
-    //         })
-    //         .catch((error) => {
-    //             console.log(error)
-    //         })
-    //     }
-    // }, [photos, img])
+            api.post('job', form)
+            .then(response => {
+                console.log(response.data)
+                setLength(-1)
+                setImg([])
+                refresh ? setRefresh(false) : setRefresh(true)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+        }
+    }, [photos, img])
 
 
 
@@ -191,11 +154,7 @@ export default function Admin() {
             <section>
                 <div>
                     <h2>Jobs</h2>
-                    <button onClick={() => {
-                        setEditData([])
-                        setModal(true)
-                            }}
-                    >
+                    <button onClick={() => setModal(true)}>
                         Adicionar
                     </button>
                 </div>
@@ -204,18 +163,11 @@ export default function Admin() {
                         <div key={item._id}>
                             <div>
                                 <h3>{item.name}</h3>
-                                <div>
-                                    <span onClick={() => EditJob(item._id)}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
-                                            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
-                                        </svg>
-                                    </span>
-                                    <span onClick={() => {DeleteJob(item._id, item.img, item.photos)}}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
-                                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                                        </svg>
-                                    </span>
-                                </div>
+                                <span onClick={() => {DeleteJob(item._id, item.img, item.photos)}}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
+                                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                                    </svg>
+                                </span>
                             </div>
                             <Image  src={item.img[1]} width='290px' height='290px' alt='Trabalhos'/>
                         </div>
@@ -232,53 +184,21 @@ export default function Admin() {
             <div id='canClose' className={styles.container} onClick={closeModal}>
                 <div className={styles.modal}>
                     <h3>Novo Serviço</h3>
-                    <form encType="multipart/form-data" onSubmit={(data) => {editData.length != 0 ? DataForm(data, 'put') : DataForm(data, 'post')}}>
+                    <form encType="multipart/form-data" onSubmit={DataForm}>
                         <label>
-                            <input type='text' placeholder='Nome' defaultValue={editData.length != 0 ? editData.name : null} required></input>
+                            <input type='text' placeholder='Nome' required></input>
                         </label>
                         <label>
-                            <input type='text' placeholder='Descrição' defaultValue={editData.length != 0 ? editData.desc : null} required></input>
+                            <input type='text' placeholder='Descrição' required></input>
                         </label>
                         <label>
                             Imagens de capa:
-                            {editData.length != 0 ?
-                                <input type='file' name='file'></input>
-                            :
-                                <input type='file' name='file' required></input>
-                            }
+                            <input type='file' name='file' required></input>
                         </label>
                         <label>
                             Imagens do serviço:
-                            {editData.length != 0 ?
-                                <input type='file' name='file[]' multiple></input>
-                            :
-                                <input type='file' name='file[]' multiple required></input>
-                            }
+                            <input type='file' name='file[]' multiple required></input>
                         </label>
-                        {editData.length != 0 ?
-                            <div className={styles.photos}>
-                                {editData.photos.map(photo => 
-                                    <>
-                                        {editData.photos.findIndex(element => element[0] == photo[0]) >= 0 ?
-                                            <div style={{backgroundImage: `url(${photo[1]})`}} key={photo[0]}>
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className={`bi bi-x ${styles.iconX}`} viewBox="0 0 16 16" onClick={() => removeImg('remove', photo[0])}>
-                                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                                                </svg>
-                                            </div>
-                                        :
-                                            <div style={{backgroundImage: `url(${photo[1]})`}} className={styles.none} key={photo[0]}>
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className={`bi bi-arrow-clockwise ${styles.iconR}`} viewBox="0 0 16 16" onClick={() => removeImg('add', photo)}>
-                                                    <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
-                                                    <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
-                                                </svg>
-                                            </div>
-                                        }
-                                    </>
-                                )}
-                            </div>
-                        :
-                            null
-                        }
                         <div>
                             <button>Cadastrar</button>
                             <button id='canClose'>Cancelar</button>
