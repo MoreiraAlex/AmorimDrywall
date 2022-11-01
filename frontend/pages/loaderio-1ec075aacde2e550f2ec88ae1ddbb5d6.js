8640291c52c1c2dbfdd9bc9d57c536e2db080444
MoreiraAlex <celerom100@gmail.com>
2022-11-01 20:33:31 -0300

    Config para teste de performace v3

--- frontend/pages/loaderio-1ec075aacde2e550f2ec88ae1ddbb5d6.js
+++
@@ -1,5 +0,0 @@
-function verify() {
-  return 'loaderio-1ec075aacde2e550f2ec88ae1ddbb5d6'
-}
-
-export default verify
---
+++ frontend/pages/loaderio-86b44d126fbd7c00d91b97acf5a6a59c.js
@@ -0,0 +1,5 @@
+function verify() {
+  return 'loaderio-86b44d126fbd7c00d91b97acf5a6a59c'
+}
+
+export default verify
