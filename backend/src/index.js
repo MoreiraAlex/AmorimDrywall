965a2bc613a223f62cb2854bce5d7afcb624539b
MoreiraAlex <celerom100@gmail.com>
2022-11-01 12:12:08 -0300

    Ajustando allow header da api v2

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,PATCH,DELETE")
     res.header(
-        "Access-Control-Allow-Header",
+        "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization" 
     )
     app.use(cors())
