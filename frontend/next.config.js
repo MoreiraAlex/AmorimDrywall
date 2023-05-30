ea9ee19815284a1649332d80a629613e8b79d83c
MooreiraAlex <amoreira@allianceconsultoria.com.br>
2023-05-30 15:13:38 -0300

    Nova versão do site

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['amorim-drywall-upload.s3.amazonaws.com', 'amorim-drywall-upload.s3.sa-east-1.amazonaws.com', 'localhost'],
-  }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
--- frontend/pages/loaderio-86b44d126fbd7c00d91b97acf5a6a59c.js
+++
@@ -1,3 +0,0 @@
-export default function loaderio_86b44d126fbd7c00d91b97acf5a6a59c(){
-    return 'loaderio-86b44d126fbd7c00d91b97acf5a6a59c'
-}
